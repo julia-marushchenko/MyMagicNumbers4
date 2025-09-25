a5418b90c346d54d165cd3fb14b9164a69d87d37
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-09-25 08:06:01 +0200

    Add files via upload
    Committing Main class

---
+++ src/com/numbers/Main.java
@@ -0,0 +1,28 @@
+// Java program to  find numbers divided by 3 without rest in range (18, 87)
+
+package com.numbers;
+
+// Main class
+public class Main {
+
+    // Main method to run java program
+    public static void main(String[] args) {
+
+        // Creating loop to go through numbers [18, 87]
+        for (int index = 19; index < 87; index++) {
+
+            // Condition
+            if (index % 3 == 0) {
+
+                // Printing result to console
+                System.out.println(index + " can be divided by 3");
+
+            } else {
+
+                // Printing result to console
+                System.out.println(index + " cannot be divided by 3");
+
+            }
+        }
+    }
+}
